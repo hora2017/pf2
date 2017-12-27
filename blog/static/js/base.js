c8a8a4c8dd700d6692e7289a9e50f80930efd6b4
hora2017 <ho_ra@naver.com>
2017-12-27 15:26:56 +0900

    base.js 수정

--- blog/static/js/base.js
+++ blog/static/js/base.js
@@ -7,21 +7,14 @@ var cssBl = {
         } else { cssBl.theme = 'true'; }
     },
     css: function () {
+        var head = document.getElementsByTagName('head')[0];
+        var link = document.createElement('link');
+        link.id = 'cssId';
+        link.rel = 'stylesheet';
         if (localStorage['theme'] === 'false') {
-            var head = document.getElementsByTagName('head')[0];
-            var link = document.createElement('link');
-            link.id = 'cssId';
-            link.rel = 'stylesheet';
             link.href = '/static/css/blog.css';
-            head.appendChild(link);
-        } else {
-            var head = document.getElementsByTagName('head')[0];
-            var link = document.createElement('link');
-            link.id = 'cssId';
-            link.rel = 'stylesheet';
-            link.href = '/static/css/blog.1.css';
-            head.appendChild(link);
-        }
+        } else { link.href = '/static/css/blog.1.css'; }
+        head.appendChild(link);
     }
 }
 
