380ff645038e1b4303368fef14e079239dfe0686
hora2017 <ho_ra@naver.com>
2018-01-24 11:52:19 +0900

    페이지네이션 추가

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,38 @@ from django.utils import timezone
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def tag_post_list(request, tag):
     tposts = Post.objects.filter(tags__name__in=[tag]).order_by('-created_date')
+    paginator = Paginator(tposts, 3)
+    page = request.GET.get('page')
+    try:
+        tposts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        tposts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        tposts = paginator.page(paginator.num_pages)
     return render(request, 'blog/tag_post_list.html', {'tposts': tposts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -27,11 +50,12 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            form.save_m2m() # 폼에서 작성시 태그 저장
-            return redirect('post_detail', pk=post.pk)
+            form.save_m2m()  # 폼에서 작성시 태그 저장
+            return redirect('post_list')
     else:
         form = PostForm()
     return render(request, 'blog/post_new.html', {'form': form})
+
 
 @login_required
 def add_comment_to_post(request, pk):
@@ -46,6 +70,7 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
 
 @login_required
 def comment_remove(request, pk):
